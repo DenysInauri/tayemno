211191473678923ae66547797167e0161c011904
Inauri <denis.inauri@gmail.com>
2026-09-11 12:04:08 +0200

    Update defaultColorScheme to dark

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -7,7 +7,7 @@ import { App } from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MantineProvider>
+    <MantineProvider defaultColorScheme="dark">
       <App />
     </MantineProvider>
   </StrictMode>,
