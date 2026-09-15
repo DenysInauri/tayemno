b3120b8bda47726f86b4be858a0a1367b469a7d7
Inauri <denis.inauri@gmail.com>
2026-09-15 15:40:26 +0200

    Added sign in page

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -4,18 +4,24 @@ import { SignInPage } from "./pages/SignInPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { VerifyEmailPage } from "./pages/VerifyEmailPage";
 import { ForgotPasswordPage } from "./pages/ForgotPasswordPage";
+import { AuthProvider } from "./contexts/AuthContext";
 
 export const App = () => (
   <BrowserRouter>
-    <Routes>
-      <Route path={RouteEnum.SIGN_IN} element={<SignInPage />} />
-      <Route path={RouteEnum.SIGN_UP} element={<RegisterPage />} />
-      <Route path={RouteEnum.VERIFY_EMAIL} element={<VerifyEmailPage />} />
-      <Route
-        path={RouteEnum.FORGOT_PASSWORD}
-        element={<ForgotPasswordPage />}
-      />
-      <Route path="*" element={<Navigate to={RouteEnum.SIGN_IN} replace />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path={RouteEnum.SIGN_IN} element={<SignInPage />} />
+        <Route path={RouteEnum.SIGN_UP} element={<RegisterPage />} />
+        <Route path={RouteEnum.VERIFY_EMAIL} element={<VerifyEmailPage />} />
+        <Route
+          path={RouteEnum.FORGOT_PASSWORD}
+          element={<ForgotPasswordPage />}
+        />
+        <Route
+          path="*"
+          element={<Navigate to={RouteEnum.SIGN_IN} replace />}
+        />
+      </Routes>
+    </AuthProvider>
   </BrowserRouter>
 );
